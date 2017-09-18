18f74bde08ded61a7fe74c38815cf7df9e65deb1
warmflasha <warmflasha@gmail.com>
2017-09-18 18:12:28 -0500

    graded inclass4

--- Inclass4.m
+++ Inclass4.m
@@ -1,3 +1,5 @@
+%AW: Looks good in general. See commments below. 0.89/1.
+
 %Inclass assignment 4. Due at the start of class on 9/12/17
 
 %1. (a) Write code that makes a file with the words "Random numbers 1" on its
@@ -25,9 +27,14 @@ fin=fopen('inclass4.txt', 'r');
 line1=fgetl(fin)
 line2=fgetl(fin)
 arr1=strsplit(line2, "\t")
+%AW: this is all correct. Note that at the end, you still have a cell array of strings. Need to run
+% something like arr1 = cellfun(@str2num,arr1) or run a loop and call str2num on each entry of arr1. -0.01
+
 % 2. Write a function that takes an array as input and returns a logical
 % variable which is true if the sum of the numbers in the array is greater
 % than or equal to 10 and false if it is less than 10. 
 
 rand_array=randi([0,5],1, 5)
 greater_10=sum(rand_array) >= 10
+
+%AW: This is the correct operation, but it isn't written as a function. -.10. 
